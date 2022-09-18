7c43395927fbd1b8c67df09848b7c8bf7fac4312
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-09-18 18:53:39 +0900

    25083 solved

---
+++ 25083/25083.js
@@ -0,0 +1,7 @@
+console.log(`         ,r'"7`);
+console.log("r`-_   ,'  ,/");
+console.log(` \\. ". L_r'`);
+console.log('   `~\\/');
+console.log('      |');
+console.log('      |');
+//'와 " 말고도 ` 도 가능하다.
