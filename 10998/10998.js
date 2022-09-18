8761b95029e2a63b810451440673da829d551a0e
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-09-18 15:32:40 +0900

    10998 solved

---
+++ 10998/10998.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+//let input = fs.readFileSync('example.txt').toString().split(' ');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let a = input[0];
+let b = input[1];
+
+console.log(a*b);
