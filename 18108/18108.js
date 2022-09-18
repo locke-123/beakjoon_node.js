496ec0c2c9c91bb6ccfe7b643a35e73ad812d606
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-09-18 16:36:34 +0900

    18108 solved

---
+++ 18108/18108.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+//let input = fs.readFileSync('example.txt').toString();
+let input = fs.readFileSync('/dev/stdin').toString();
+
+let a = parseInt(input);
+
+console.log(a-543);
