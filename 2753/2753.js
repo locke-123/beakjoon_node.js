7866760372fdc84395aa706caea086afe3e2df18
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-09-18 19:27:29 +0900

    2753 solved

---
+++ 2753/2753.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+//let input = parseInt(fs.readFileSync('example.txt').toString());
+let input = parseInt(fs.readFileSync('/dev/stdin').toString());
+
+if(input%4 === 0 && (input%100 !== 0 || input%400 === 0)){
+    console.log(1);
+} else {
+    console.log(0);
+}
