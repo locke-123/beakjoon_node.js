584d80ea37c684dc7224807cf7948360ca08ac0e
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-09-20 21:59:46 +0900

    1065 in progress

---
+++ 단계별로 풀어보기/5단계 함수/1065/1065.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let input = parseInt(fs.readFileSync('example.txt').toString());
+console.log(input);
+
+function ap(n) { //등차수열 판별기
+    //각 자릿수 분해 4673 참조
+    //각 자릿수를 배열로 할당
+    //배열의 각 두 수의 차이가 일정한지 확인
+}
