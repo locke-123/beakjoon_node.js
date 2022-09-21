c0644f2b2c59efb79147eeed3fddddb3ea34bc87
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-09-21 13:11:50 +0900

    10809 in progress

---
+++ 단계별로 풀어보기/6단계 문자열/10809/10809.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let input = fs.readFileSync('example.txt').toString();
+console.log(input);
+
+let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+'p','q','r','s','t','u','v','w','x','y','z'];
+
+for(let i=0; i<input.length; i++){
+    for(let j=0; j<alphabet.length; j++){
+        if(input[i] === )
+    }
+}
