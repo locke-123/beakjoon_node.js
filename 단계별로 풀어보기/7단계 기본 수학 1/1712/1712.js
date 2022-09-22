f4e0c79a5bca8bb85eea81b1824fa34272f08e92
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-09-22 13:10:16 +0900

    1712 in progress

--- 단계별로 풀어보기/7단계 기본 수학 1/1712/1712.js
+++ 단계별로 풀어보기/7단계 기본 수학 1/1712/1712.js
@@ -1,1 +1,23 @@
 const fs = require('fs');
+//let input = fs.readFileSync('/dev/stdin').toString().split(' ').map(s => parseInt(s));
+let input = fs.readFileSync('example.txt').toString().split(' ').map(s => parseInt(s));
+
+let A = input[0];
+let B = input[1];
+let C = input[2];
+
+let i = 0;
+let income = 0;
+let cost = A;
+
+while(cost > income){
+    if(B >= C){
+        i = -1;
+        break;
+    }
+    i++;
+    income += C;
+    cost += B;
+}
+
+console.log(i);
