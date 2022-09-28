ac25c88f5894c1d540b627f1150b16f3b4e7d970
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-09-28 12:26:14 +0900

    2750 solved

--- 단계별로 풀어보기/9단계 정렬/2750/2750.js
+++ 단계별로 풀어보기/9단계 정렬/2750/2750.js
@@ -1,1 +1,11 @@
 const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+//let input = fs.readFileSync('example.txt').toString().trim().split('\n').map(Number);
+let N = input[0];
+input.shift();
+
+input.sort((a,b) => a-b); //오름차순 sort 내장 객체
+
+for(i of input){
+    console.log(i);
+}
