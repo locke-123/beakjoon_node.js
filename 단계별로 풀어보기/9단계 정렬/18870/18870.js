efca8f65bcd2d4612af5d4954f94905de4c59162
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-10-08 20:55:52 +0900

    18870 solved & update

--- 단계별로 풀어보기/9단계 정렬/18870/18870.js
+++ 단계별로 풀어보기/9단계 정렬/18870/18870.js
@@ -5,25 +5,17 @@ let N = parseInt(input[0]);
 input.shift();
 input = input[0].split(' ').map(s => parseInt(s));
 
-//ex 2 4 -10 4 -9
+let numberSet = new Set(input); //set을 이용해서 생성하면 중복이 제거된다.
+let number = [...numberSet]; //다시 set에서 배열로 할당
 
-//input안에 각각의 숫자들 가져오기 중복은 X
-//ex 2 4 -10 -9
-let numberSet = new Set(input);
-let number = [...numberSet];
-
-//각 숫자들마다 가장 작은 값 부터 0을 매기고 1씩 키우기
-//ex 2 = 2   4 = 3  -10 = 0  -9 = 1
+//각 숫자들을 작은 수 부터 정렬함으로써 index가 0부터 시작하게 하기
 number.sort((a,b) => a-b);
 
-//각각 맵핑된 수들을 기존의 배열에 replace 하기
-//ex 2 3 0 3 1
-
+//find라는 배열에 각 ele의 index에 기존 index(압축된 좌표)를 저장
 let find = [];
-number.forEach((ele, index) => find[ele] = index);
-let ans = '';
+number.forEach((ele, index) => find[ele] = index); //number의 내용과 index를 각 할당
+let ans = ''; //출력
 for(let i=0; i<N; i++){
     ans += find[input[i]]+' ';
 }
 console.log(ans);
-//출력
