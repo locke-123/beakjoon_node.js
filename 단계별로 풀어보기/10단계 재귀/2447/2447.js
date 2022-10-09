f5a32ca331ebb015be75b2af5c0e097c8a77256a
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-10-09 23:53:34 +0900

    2447 solved

--- 단계별로 풀어보기/10단계 재귀/2447/2447.js
+++ 단계별로 풀어보기/10단계 재귀/2447/2447.js
@@ -1,20 +1,25 @@
 const fs = require('fs');
-let input = parseInt(fs.readFileSync('example.txt').toString().trim());
-console.log(input);
-let ans = recursion(9);
+let input = parseInt(fs.readFileSync('/dev/stdin').toString().trim());
+//let input = parseInt(fs.readFileSync('example.txt').toString().trim());
 
-function recursion (n) {
-    let txt = [];
-    if(n===1){
-        txt.push('*');
-        txt.push('*');
-        return txt;
-    }
-    let txt2 = [];
-    txt = recursion(n/3);
-    txt2.push(txt[0].repeat(3));
-    txt2.push(txt[1].repeat(n/3)+' '.repeat(n/3)+txt[1].repeat(n/3));
-    console.log(txt2,n);
-    return txt2;
+// 이문제 다시 복습 & 분석
+//console.log('answer ='+recursion(3,4,9)+';');
+
+function recursion (i,j,n) {
+    //console.log(i,j,n);
+    if(i%3 === 1 && j%3 === 1) return ' ';
+    else if(n === 1) return '*';
+    else return recursion(Math.floor(i/3),Math.floor(j/3),n/3);
 }
+
+
+let ans = '';
+
+for(let i=0; i<input; i++){
+    for(let j=0; j<input; j++){
+        ans += recursion(i,j,input);
+    }
+    ans += '\n';
+}
+
 console.log(ans);
