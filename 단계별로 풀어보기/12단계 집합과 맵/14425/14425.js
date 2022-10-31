ba2857a83fbeec4821cab8496bd5a1d51632299a
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-10-31 15:00:43 +0900

    Update 14425.js

--- 단계별로 풀어보기/12단계 집합과 맵/14425/14425.js
+++ 단계별로 풀어보기/12단계 집합과 맵/14425/14425.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(s => s.trim());
-//let input = fs.readFileSync('example.txt').toString().trim().split('\n').map(s => s.trim());
+//let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(s => s.trim());
+let input = fs.readFileSync('example.txt').toString().trim().split('\n').map(s => s.trim());
 //집합 S 는 N, 검사할 항목은 M개로 주어진다.
 let [N,M] = input[0].split(' ').map(Number);
 input.shift();
@@ -8,7 +8,29 @@ input.shift();
 
 //일일히 검사하는 방법?
 //S 정렬 후 b-search 사용 하는 방법?
+let S = input.splice(0,N);
 
+S.sort();
+for(let j=0; j<input.length; j++){
+    if(b_search(input[j])){
+        count++;
+    }
+}
+
+function b_search(str) {
+    let m = Math.floor(S.length-1/2);
+    if()
+    
+    if(str.includes(S[m])) {
+        return true;
+    } else if(str[0] > S[m][0]){
+        return b_search(str, start, m);
+    } else {
+        return b_search(str, m, end);
+    }
+}
+
+/*
 let count = 0;
 for(let i=0; i<N; i++){
     //console.log('i = ' + i);
@@ -25,4 +47,5 @@ for(let i=0; i<N; i++){
 }
 console.log(count);
 
-//제출해보기
+==> 시간 초과
+*/
