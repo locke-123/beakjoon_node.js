6a3d41199df97607bd671d03447411ac04790e51
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-11-03 13:08:33 +0900

    2477 in progress

--- 단계별로 풀어보기/14단계 기하1/2477/2477.js
+++ 단계별로 풀어보기/14단계 기하1/2477/2477.js
@@ -1,3 +1,26 @@
 const fs = require('fs');
+//let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(s => s.trim());
 let input = fs.readFileSync('example.txt').toString().trim().split('\n').map(s => s.trim());
-console.log(input);
+let N = parseInt(input[0]); input.shift();
+
+let count = new Array(4).fill(0);
+for(let i=0; i<input.length; i++){
+    input[i] = input[i].split(' ').map(Number);
+    count[input[i][0]-1] += 1;
+}
+console.log(input,count);
+/*
+4 50
+2 160
+3 30
+1 60  --
+3 20  --
+1 100
+
+3 20 --
+1 100
+4 50    1
+2 160   1
+3 30
+1 60  --
+*/
