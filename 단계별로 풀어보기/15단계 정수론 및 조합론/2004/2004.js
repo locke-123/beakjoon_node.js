001754c47d44f4a3111ec01b38e8a7f4dae88ca4
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-11-15 15:07:30 +0900

    2004 solved

--- 단계별로 풀어보기/15단계 정수론 및 조합론/2004/2004.js
+++ 단계별로 풀어보기/15단계 정수론 및 조합론/2004/2004.js
@@ -1,23 +1,29 @@
 const fs = require('fs');
-//let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
-let input = fs.readFileSync('example.txt').toString().trim().split(' ').map(Number);
+let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+//let input = fs.readFileSync('example.txt').toString().trim().split(' ').map(Number);
 let [n,k] = input;           //     n!          각각의 2와 5의 배수를 구하면된다.
-console.log(n,k);            //   k!(n-k)!
+//console.log(n,k);            //   k!(n-k)!      +1676 도 이와 같은 방법으로 구하기 가능 한번 해보기
 
-console.log(getTwoFive(30));
+let nNum = getTwoFive(n);
+let kNum = getTwoFive(k);
+let n_kNum = getTwoFive(n-k);
 
-function getTwoFive(n){
+let twoNum = nNum[0] - (kNum[0] + n_kNum[0]);
+let fiveNum = nNum[1] - (kNum[1] + n_kNum[1]);
+
+console.log(Math.min(twoNum,fiveNum));
+
+function getTwoFive(n){ //원리는 정확히 파악 못함. 관련정보 공부
     let ans = [];
     let n2 = n;
     let n5 = n;
     let ans2 = 0;
-    while(n2 >= 2){
+    while(n2 >= 2){ //n! 안에서 2의 갯수 구하기
         ans2 += parseInt(n2/2);
-        console.log(n2);
         n2 /= 2;
     }
     ans.push(ans2);
-    let ans5 = 0;
+    let ans5 = 0;  //5의 갯수 구하기
     while(n5 >= 5){
         ans5 += parseInt(n5/5);
         n5 /= 5;
