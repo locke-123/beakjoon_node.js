46d633f0f339010f5297cce746309c9fedc68678
locke-123 <113772136+locke-123@users.noreply.github.com>
2022-11-30 15:48:22 +0900

    1932 solved

--- 단계별로 풀어보기/17단계 동적 계획법1/1932/1932.js
+++ 단계별로 풀어보기/17단계 동적 계획법1/1932/1932.js
@@ -1,19 +1,35 @@
 const fs = require('fs');
-let input = fs.readFileSync('example.txt').toString().trim().split('\n').map(s=>s.trim());
-console.log(input);
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(s=>s.trim());
+//let input = fs.readFileSync('example.txt').toString().trim().split('\n').map(s=>s.trim());
+//console.log(input);
 let N = parseInt(input[0]);
 let memo = [];
-memo[1] = parseInt(input[1]);
-memo[2] = [10,15]
-for(let i=3; i<input.length; i++){
-    let arr  = input[i].split(' ').map(Number);
-    for(let j=0; j<memo[i-1].length; j++){
-        memo[i] = [];
-        memo[i].push(memo[i-1][j] + arr[j]);
-        memo[i].push(memo[i-1][j] + arr[j+1]);
-    }
+
+if(N === 1){
+  memo[1] = parseInt(input[1]);
+  console.log(memo[1]);
+} else if(N===2){
+  memo[1] = parseInt(input[1]);
+  memo[2] = input[2].split(' ').map(s => parseInt(s)+memo[1]);
+  console.log(Math.max(...memo[N]));
+} else {
+  memo[1] = parseInt(input[1]);
+  memo[2] = input[2].split(' ').map(s => parseInt(s)+memo[1]);
+  for(let i=3; i<input.length; i++){ //i는 단계를 나타냄
+      let arr  = input[i].split(' ').map(Number);
+      memo[i] = [];
+      for(let j=0; j<i; j++){ //j는 각 단계 index
+        if(j===0){
+          memo[i].push(memo[i-1][0] + arr[j]); //왼쪽 모서리는 이전단계 첫번째와 현 첫 index 합
+        } else if (j === i-1) {
+          memo[i].push(memo[i-1][j-1] + arr[j]); //마지막 모서리는 이전 마지막 index와 현 마지막 index
+        } else {
+          memo[i].push(Math.max(memo[i-1][j-1] + arr[j],memo[i-1][j] + arr[j])); //중간 2가지 선택지의 경우 이전의 왼쪽과 현재 index합중 최대가 max
+        }
+      }
+  }
+  console.log(Math.max(...memo[N]));
 }
-console.log(memo);
 /*
 5
 7
@@ -23,6 +39,12 @@ console.log(memo);
 4 5 2 6 5
 
 30
+
+          7
+        10  15
+    18    16  15
+  20   25    20   19
+24   30   27    26   24
 */
 
 
